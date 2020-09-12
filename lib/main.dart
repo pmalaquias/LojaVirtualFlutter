40ecb91ab9ab8ac5d4b5d322a5351062eea0e3b9
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2020-09-12 15:30:55 -0300

    Navegando entre as paginas

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual_flutter/screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +10,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      title: "Flutter's Clothing",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Color.fromARGB(255, 4, 125, 141),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
