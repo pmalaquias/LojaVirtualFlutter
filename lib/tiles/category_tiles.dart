ddd0ab36866613e6a80167e2b27d2d5b0eccefc0
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2020-09-27 13:48:20 -0300

    Mostrando lista de categorias

---
+++ lib/tiles/category_tiles.dart
@@ -0,0 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class CategoryTiles extends StatelessWidget {
+
+  final DocumentSnapshot snapshot;
+
+  CategoryTiles(this.snapshot);
+  
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar( //parte que fica no inicio
+        radius: 25.0,
+        backgroundColor: Colors.transparent,
+        backgroundImage: NetworkImage(snapshot.data["icon"]),
+      ),
+      title: Text(snapshot.data["title"]),
+      trailing: Icon(Icons.keyboard_arrow_right),//parte que fica no final
+      onTap: (){
+
+      },
+    );
+  }
+}
