dba90c426c01aa65c55209f79088a50ce491134c
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2020-10-21 14:58:21 -0300

    Criando a Tile da lista

--- lib/tiles/product_tile.dart
+++ lib/tiles/product_tile.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual_flutter/datas/product_data.dart';
+import 'package:loja_virtual_flutter/screens/product_screen.dart';
 
 class ProductTile extends StatelessWidget {
-
   final String type;
   final ProductData product;
 
@@ -11,47 +11,84 @@ class ProductTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      onTap: (){
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context)=>ProductScreen(product))
+        );
+      },
       child: Card(
-        child: type == "grid" ?
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              AspectRatio(
-                  aspectRatio: 0.8,
-                  child: Image.network(
-                    product.images[0],
-                    fit: BoxFit.cover,
-                  ),
-              ),
-              Expanded(
-                  child: Container(
-                    padding: EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          product.title,
-                          style: TextStyle(
-                            fontWeight: FontWeight.w500
-                          ),
-                        ),
-                        Text(
-                          "R\$ ${product.price.toStringAsFixed(2)}",
-                          style: TextStyle(
-                            color: Theme.of(context).primaryColor,
-                            fontSize: 17.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
+        child: type == "grid"
+            ? Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  AspectRatio(
+                    aspectRatio: 0.8,
+                    child: Image.network(
+                      product.images[0],
+                      fit: BoxFit.cover,
                     ),
                   ),
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            product.title,
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            "R\$ ${product.price.toStringAsFixed(2)}",
+                            style: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              fontSize: 17.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            : Row(
+          children: <Widget>[
+            Flexible(
+              flex: 1,
+              child: Image.network(
+                product.images[0],
+                fit: BoxFit.cover,
+                height: 250.0,
               ),
-            ],
-          )
-          : Row(),
-      ) ,
+            ),
+            Flexible(
+              flex: 1,
+              child:Container(
+                padding: EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      product.title,
+                      style: TextStyle(fontWeight: FontWeight.w500),
+                    ),
+                    Text(
+                      "R\$ ${product.price.toStringAsFixed(2)}",
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontSize: 17.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
