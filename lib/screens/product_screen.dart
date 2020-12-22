df76f7d052cb91af77772c83fb48e612d3ccd1a1
Pedro Igor <pedrode13souza@gmail.com>
2020-12-22 10:23:56 -0300

    Criando a tela do Produto

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,7 +1,8 @@
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 import 'package:loja_virtual_flutter/datas/product_data.dart';
-class ProductScreen extends StatefulWidget {
 
+class ProductScreen extends StatefulWidget {
   final ProductData product;
 
   ProductScreen(this.product);
@@ -11,13 +12,59 @@ class ProductScreen extends StatefulWidget {
 }
 
 class _ProductScreenState extends State<ProductScreen> {
-
   final ProductData product;
 
   _ProductScreenState(this.product);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final Color primaryColor = Theme.of(context).primaryColor;
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(product.title),
+          centerTitle: true,
+        ),
+        body: ListView(
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 0.9,
+              child: Carousel(
+                images: product.images.map((url) {
+                  return NetworkImage(url);
+                }).toList(),
+                dotSize: 4.0,
+                dotSpacing: 15.0,
+                dotBgColor: Colors.transparent,
+                dotColor: primaryColor,
+                autoplay: false,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    product.title,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    maxLines: 3,
+                  ),
+                  Text(
+                    'R\$ ${product.price.toStringAsFixed(2)}',
+                    style: TextStyle(
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.bold,
+                      color: primaryColor,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
